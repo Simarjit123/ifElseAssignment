f9d130863a655b80a360aaec98d607f8eaab6108
Simarjit123 <96345005+Simarjit123@users.noreply.github.com>
2022-01-04 11:10:51 -0500

    Add files via upload

---
+++ question5.java
@@ -0,0 +1,18 @@
+package ifElse;
+
+public class question5 {
+
+	public static void main(String[] args) {
+//value of sales
+		double sales = 77.5;
+		if (sales <= 50) {
+			System.out.println("1% commission recieved");
+		} else if (sales <= 70) {
+			System.out.println("5% commission recieved");
+		} else {
+			System.out.println("10% commission recievedclear");
+		}
+
+	}
+
+}
